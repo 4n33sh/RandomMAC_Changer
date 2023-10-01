01accaef8edf7d92a881ffe9d5bef44b2700122c
Aneesh Sai Praneeth M <71321103+4n33sh@users.noreply.github.com>
2023-10-01 15:05:19 +0530

    Primary MAC Address Alter code upload
    This code is basic in nature and does not implement randomization for ethernet network interfaces and utilizes macchanger to randomise NIC MAC. Caution is recommended before implementation of this program.

---
+++ MACchange.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+echo "What type of network do you wish to randomise?"
+echo " "
+echo "#1. eth"
+echo "#2. wlan"
+echo " "
+read input
+
+if [[ $input == 1 ]]
+then
+	echo " "
+	echo "Enter the ethernet device you want to randomise: (e.g. eth0 [or] eth1 [or] eth2)"
+	echo " "
+	read type_eth
+
+	command1=`ifconfig | grep $type_eth`
+
+	if [ -z "$command1" ];
+	then
+	   	echo "No eth0 connection! Failed to randomise MAC of eth0."
+		echo " "
+		echo "Troubleshooting: "
+		echo "1. Please try entering an existing ethernet connection (i.e. check your input)"
+		echo "2. Please try checking the connection of ethernet cable to your computer/VM is established."
+	else
+
+		mac1="00:11:22:33:44:55"
+
+	    	sudo ifconfig $type_eth down
+		sudo ifconfig $type_eth hw ether $mac1
+		sudo ifconfig $type_eth up
+		echo " "
+		echo "Changed MAC of $type_eth ."
+		echo "Current MAC of $type_eth is: $mac1"
+	fi
+
+else
+	echo " "
+	echo "Enter the wlan/NIC device you want to randomise: (e.g. wlan0 [or] wlan1 [or] wlan2)"
+	echo " "
+	read type_wlan
+	echo " "
+
+	command2=`ifconfig | grep $type_wlan`
+
+	if [ -z "$command2" ];
+	then
+   	 	echo "wlan0/NIC Adapter not detected. Failed to randomise MAC of wlan0."
+		echo " "
+		echo "Troubleshooting: "
+		echo "1. Please try entering an existing NIC (i.e. check your input)"
+		echo "2. Please try checking the connection of NIC to your computer/VM is established."
+		echo " "
+	else
+		echo "MAC Change (wlan0) status: "
+		echo " "
+	   	sudo macchanger -r $type_wlan
+		echo " "
+		echo "Changed MAC of NIC/$type_wlan"
+	fi
+fi
